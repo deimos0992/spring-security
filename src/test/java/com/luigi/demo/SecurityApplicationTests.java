969fad9af84c1fb50c60f0b06ec79ec30662518b
deimos <luigi_benvenuto@live.it>
2022-11-02 10:48:37 +0100

    Initial commit

---
+++ src/test/java/com/luigi/demo/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luigi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
